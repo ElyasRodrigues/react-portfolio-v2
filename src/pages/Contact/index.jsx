e62fc3f4f6c1835d8f52003a80a5a72b70f4be4d
Elyas Rodrigues <elyasrz11@gmail.com>
2023-02-13 16:44:33 -0300

    inclusion of contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,5 +1,71 @@
+import { useState } from "react"
+
+import "./contact.css"
+
+
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "" 
+  })
+
+  function handleChange (ev) {
+
+    const {name, value} = ev.target
+
+    setFormData((prev) => {
+      const newData = {...prev, [name]: value}
+      return newData
+    })
+
+  }
+
+  function  handleSubmit(ev) {
+    ev.preventDefault()
+    console.log(formData);
+  }
+
   return (
-    <h1>Contact</h1>
+    <div className="container">
+      <h1>Contato</h1>
+      <div className="form-contact">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome:</label>
+          <input 
+            type="text" 
+            name="name" 
+            id="name" 
+            placeholder="Digite seu nome.."
+            value={formData.name}
+            onChange={handleChange}
+
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="informe seu email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="message">Mensagem:</label>
+          <textarea 
+            name="message" 
+            id="message" 
+            cols="30" 
+            rows="10" 
+            placeholder="Digite sua mensagem.."
+            value={formData.message}
+            onChange={handleChange}
+          ></textarea>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
   )
 }
--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -10,69 +10,70 @@ const projects = [
     id: 2,
     name: "R Link's",
     img: "https://images2.imgbox.com/28/a5/G1N4uBF7_o.png",
-    description: "",
+    description: "Projeto desenvolvido em React Js no Workshop do Sujeito Programador para armazenar diversos links, nele usamos: Hooks, Rotas (com área privada), Sistema de Login/autenticação (Firebase) e Banco de dados (Firebase)",
     github: "https://github.com/ElyasRodrigues/devLink",
     inWeb: "https://rlinks.vercel.app/"
   },{
     id: 3,
     name: "Controle de finanças",
     img: "https://images2.imgbox.com/3e/1b/sFVigLHo_o.png",
-    description: "",
+    description: "App Web desenvolvido para aprimorar meu conhecimento em React Js e Local Storage.",
     github: "https://github.com/ElyasRodrigues/finance-control",
     inWeb: "https://er-finance.vercel.app/"
   },{
     id: 4,
     name: "TODO List",
     img: "https://images2.imgbox.com/94/e3/yEXtQRHn_o.png",
-    description: "",
+    description: "App Web desenvolvido para aprimorar técnicas e boas práticas no Javascript.",
     github: "https://github.com/ElyasRodrigues/todoList",
     inWeb: "https://elyasrodrigues.github.io/todoList/"
   },{
     id: 5,
     name: "Calc Js",
     img: "https://images2.imgbox.com/08/bb/JXWEo2ny_o.png",
-    description: "",
+    description: "Projeto desenvolvido durante o módulo de Javascript do curso One Bit Code como forma de fixar o conteúdo aprendido até então.",
     github: "https://github.com/ElyasRodrigues/calcJs",
     inWeb: "https://elyasrodrigues.github.io/calcJs"
   },{
     id: 6,
     name: "Tic Tac Toe",
     img: "https://images2.imgbox.com/4e/fa/QtVmYjB6_o.png",
-    description: "",
+    description: "Esse projeto foi um exercício proposto em um módulo de Javascript do curso One Bit Code, com o intuito de fixar/aprimorar o contéudo aprendido durante o módulo, como por exemplo: Arrays, manipulação de DOM e boas práticas no JS.",
     github: "https://github.com/ElyasRodrigues/calcJs",
     inWeb: "https://elyasrodrigues.github.io/calcJs"
   },{
     id: 7,
     name: "LP - God of War",
     img: "https://images2.imgbox.com/0d/ef/OxJlr9UQ_o.png",
-    description: "",
+    description: "Página estática com o tema do jogo God of War Ragnarök, ela foi desenvolvida em HTML5, CSS3 e um pouco de JS (para manipular os slides no final da página) ",
     github: "https://github.com/ElyasRodrigues/god-of-war",
     inWeb: "https://elyasrodrigues.github.io/god-of-war/"
   },{
     id: 8,
     name: "LP - OneBitX",
     img: "https://images2.imgbox.com/f3/d1/AuFL7gu1_o.png",
-    description: "",
+    description: "Página estática proposta como um exercício no módulo de Bootstrap do curso One Bit Code, com o intuito de fixar o conhecimento adquirido durante todo o módulo.",
     github: "https://github.com/ElyasRodrigues/oneBitX",
     inWeb: "https://elyasrodrigues.github.io/oneBitX"
   },{
     id: 9,
     name: "Avengers Memory Game",
     img: "https://images2.imgbox.com/67/e8/u2hxgAa5_o.png",
-    description: "",
+    description: "Jogo da memória com o Tema dos Vingadores, projeto desenvolvido em Javascript puro, nele foi abordado vários conceitos do JS, incluindo o DOM.",
     github: "https://github.com/ElyasRodrigues/AvengersMemoryGame",
     inWeb: "https://elyasrodrigues.github.io/AvengersMemoryGame"
   },{
     id: 10,
     name: "QrCode Generate",
     img: "https://images2.imgbox.com/6c/cc/tvgCSuOr_o.png",
-    description: "",
+    description: "Projeto desenvolvido no intuido de aprimorar o conhecimento em uso de bibliotecas. Nesse foi usado o React Qr Code e Qr Code",
     github: "https://github.com/ElyasRodrigues/qrGenerate",
     inWeb: "https://qrgenerate.vercel.app/"
   },
   {
     id: 11,
     name: "Em breve...", 
+    description: "Estou desenvolvendo, pegue um cafézinho e aguarde :D",
     img: "https://images2.imgbox.com/f1/4f/5m1hqyMq_o.png",
   },
 ]
